e1f30e2521638ebf94d8532ac574f116f1c28bc0
Russell Purinton <russell.purinton@icloud.com>
2022-05-16 06:15:30 +0000

    tabulate

--- bot.php
+++ bot.php
@@ -7,16 +7,16 @@ use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
 
 $discord = new Discord([
-    'token' => trim(file_get_contents(__DIR__."/token.txt")),
+	'token' => trim(file_get_contents(__DIR__."/token.txt")),
 ]);
 
 $discord->on('ready', function (Discord $discord) {
-    echo "Bot is ready!", PHP_EOL;
+	echo "Bot is ready!", PHP_EOL;
 
-    // Listen for messages.
-    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        echo "{$message->author->username}: {$message->content}", PHP_EOL;
-    });
+	// Listen for messages.
+	$discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+	    echo "{$message->author->username}: {$message->content}", PHP_EOL;
+	});
 });
 
 $discord->run();
