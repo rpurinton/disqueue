5cd126a7c20f16dfc1f1ee917e5b13a42a36961d
Russell Purinton <russell.purinton@icloud.com>
2022-05-16 06:49:52 +0000

    adding bunny

--- bot.php
+++ bot.php
@@ -1,20 +1,24 @@
 <?php
 
 include __DIR__.'/vendor/autoload.php';
+
 use Bunny\Client;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Event;
 
 $connection = [
-    'host'      => '127.0.0.1',
-    'vhost'     => '/',    // The default vhost is /
-    'user'      => 'rabbit', // The default user is guest
-    'password'  => 'rabbit', // The default password is guest
+	'host'      => '127.0.0.1',
+	'vhost'     => '/',
+	'user'      => 'rabbit',
+	'password'  => 'rabbit',
 ];
 
 $bunny = new Client($connection);
 $bunny->connect();
+$channel = $bunny->channel();
+$channel->queueDeclare('disqueue_receive');
+$channel->queueDeclare('disqueue_send');
 
 $discord = new Discord([
 	'token' => trim(file_get_contents(__DIR__."/token.txt")),
@@ -23,9 +27,10 @@ $discord = new Discord([
 $discord->on('ready', function (Discord $discord) {
 	echo "Bot is ready!", PHP_EOL;
 
-	// Listen for messages.
 	$discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-	    echo "{$message->author->username}: {$message->content}", PHP_EOL;
+		global $channel;
+		$channel->publish($message,[],'','disqueue_receive');
+		echo "{$message->author->username}: {$message->content}", PHP_EOL;
 	});
 });
 
