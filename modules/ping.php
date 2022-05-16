f1f54057356723a28bee62a95c701cd23932bd42
Russell Purinton <russell.purinton@icloud.com>
2022-05-16 20:52:49 +0000

    example

--- modules/ping.php
+++ modules/ping.php
@@ -3,6 +3,15 @@ $functions[] = function ($data, $mq)
 {
 	if($data->t === "MESSAGE_CREATE" && $data->d->content === "ping")
 	{
-		$mq->publish("pong",[],"","disqueue_send");
+		$mq->publish(make_reply($data,"pong"),[],"","disqueue_send");
 	}
 };
+
+function make_reply($data, $content)
+{
+	$response["content"] = $content;
+	$response["reply-to"] = $data->d->id;
+	$response["guild_id"] = $data->d->guild_id;
+	$response["channel_id"] = $data->d->channel_id;
+	return json_encode($response);
+}
