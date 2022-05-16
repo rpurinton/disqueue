0649b7048765d2f58e679c4673c73f053701f72a
Russell Purinton <russell.purinton@icloud.com>
2022-05-16 06:10:58 +0000

    basic example

---
+++ bot.php
@@ -0,0 +1,22 @@
+<?php
+
+include __DIR__.'/vendor/autoload.php';
+
+use Discord\Discord;
+use Discord\Parts\Channel\Message;
+use Discord\WebSockets\Event;
+
+$discord = new Discord([
+    'token' => trim(file_get_contents(__DIR__."/token.txt")),
+]);
+
+$discord->on('ready', function (Discord $discord) {
+    echo "Bot is ready!", PHP_EOL;
+
+    // Listen for messages.
+    $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+        echo "{$message->author->username}: {$message->content}", PHP_EOL;
+    });
+});
+
+$discord->run();
