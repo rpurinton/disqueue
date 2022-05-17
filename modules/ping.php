b62f10500727e5109e11b86b612d4d81edc3cb04
Russell Purinton <russell.purinton@icloud.com>
2022-05-17 00:16:57 +0000

    commit

--- modules/ping.php
+++ modules/ping.php
@@ -1,17 +1,10 @@
 <?php
 $functions[] = function ($data, $mq)
 {
-	if($data->t === "MESSAGE_CREATE" && $data->d->content === "ping")
+	if($data->t === "MESSAGE_CREATE" && strtolower($data->d->content) === "ping")
 	{
-		$mq->publish(make_reply($data,"pong"),[],"","disqueue_send");
+		$payload["data"] = $data;
+		$payload["MessageBuilder"] = Discord\Builders\MessageBuilder::new()->setContent("PONG!");
+		$mq->publish(json_encode($payload),[],"","disqueue_send");
 	}
 };
-
-function make_reply($data, $content)
-{
-	$response["content"] = $content;
-	$response["reply-to"] = $data->d->id;
-	$response["guild_id"] = $data->d->guild_id;
-	$response["channel_id"] = $data->d->channel_id;
-	return json_encode($response);
-}
